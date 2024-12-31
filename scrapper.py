a14ef733aa804fa07aa5b2a8147ce660c9fc5c2b
Ramesh Pradhan <pyrameshpradhan@gmail.com>
2024-12-31 14:40:43 +0545

    Add git tutorial codes

--- scrapper.py
+++ scrapper.py
@@ -17,6 +17,29 @@ from bs4 import BeautifulSoup
 # git add . # file track garxa
 # git commit -m "message" # k kam gareko xa
 
+
+# install git
+# create repository in github
+
+# go to git bash
+# git config --global user.name "Ramesh Pradhan"
+# git config --global user.email "pyrameshpradhan@gmail.com"
+
+# git init
+# git status => if you want to check what are the status of files
+# git diff => if you want to check what are the changes
+# git add .
+# git commit -m "Your message"
+# copy paste git code from github
+
+
+###################################
+# 1. change the code
+# 2. git add .
+# 3. git commit -m "Your message"
+# 4. git push origin
+###################################
+
 URL = "http://books.toscrape.com/"
 
 
